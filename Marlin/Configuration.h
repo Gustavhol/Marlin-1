a1c890502137074c797a3db77dcf7793950b0438
Gustav Holmsten <gustav.holmsten@blinfo.se>
2020-07-08 21:06:58 +0200

    changes to accomodate dual gear extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,14 +730,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 189.72 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 840 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 8, 40 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
